5b87f8f207485f995cda8d7b429c698ab99f2381
Brian Jemilo II <jemiloii@jemiloii.com>
2013-10-31 15:52:37 -0500

    Update on project

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,12 +3,28 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   def new
+    @restaurant = Restaurant.new
   end
   def create
     @restaurant = Restaurant.new(params[:restaurant].permit(:name, :description, :full_address, :phone_number))
 	
-	@restaurant.save
-	redirect_to @restaurant
+	if @restaurant.save
+	  redirect_to @restaurant
+	else
+	  render 'new'
+	end
+  end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def update
+    @restaurant = Restaurant.find(params[:id])
+	
+	if @restaurant.update(params[:restaurant].permit(:name, :description, :full_address, :phone_number))
+	  redirect_to @restaurant
+	else
+	  render 'edit'
+	end
   end
   def show
     @restaurant = Restaurant.find(params[:id])
